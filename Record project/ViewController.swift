43a4d9c1ce9777b403f8ad02b4217fe2c1998a49
Sayed <sayed@sayeds-macbook-pro.local>
2019-03-17 13:21:25 +0200

    Initial Commit

---
+++ Record project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Record project
+//
+//  Created by Sayed  on 3/17/19.
+//  Copyright © 2019 Sayed . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
